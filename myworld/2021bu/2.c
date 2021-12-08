26546a0a0d737a69bb71d086fb61ec26b7277278
TGRD <88685494+TGRD4@users.noreply.github.com>
2021-12-08 16:01:48 +0800

    Delete myworld/2021bu directory

--- myworld/2021bu/2.c
+++
@@ -1,8 +0,0 @@
-int main(int argc, char *argv[])
-{
-    char *str = "Xi You Linux Group 20";
-    printf("%d\n", printf(str));
-    /*printf输出函数返回值是其成功打印字符的长度
-    此题内printf返回21，最后输出str+21*/
-    return 0;
-}//Xi You Linux Group 2021
