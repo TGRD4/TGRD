f4ccec489410ea4986ecc6c1c39154988d328214
TGRD <88685494+TGRD4@users.noreply.github.com>
2021-12-21 14:59:04 +0800

    Add files via upload

--- myworld/DP/121.c
+++ myworld/DP/121.c
@@ -1,3 +1,4 @@
+/*股票最大收益*/
 int maxProfit(int* prices, int pricesSize)
 {
     int min = prices[0],max = 0;
@@ -6,7 +7,18 @@ int maxProfit(int* prices, int pricesSize)
         if(min > prices[i])
             min = prices[i];//保留买入股票最便宜的价格
         if(prices[i] - min > max)
-            max = prices[i] - min;//保留卖出股票最贵的价格
+            max = prices[i] - min;//保留卖出股票最大的收益
+    }
+    return max;
+}
+/*股票最大收益*/
+int maxProfit(int* prices, int pricesSize)
+{
+    int min = prices[0],max = 0;
+    for(int i = 0;i < pricesSize;i++)
+    {
+        min = fmin(prices[i],min);//保留买入股票最便宜的价格
+        max = fmax(max,prices[i] - min);//保留卖出股票最大的收益
     }
     return max;
 }
